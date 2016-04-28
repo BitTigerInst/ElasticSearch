cd2e375a45f32b9e062db336bc5fd9680ee377c8
git <luoxinran28@gmail.com>
2016-04-27 21:47:54 -0600

    [Task 6] - Add Express Configuration; File Structure Assets

---
+++ config/env/default.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  app: {
+    title: 'Elasticsearch Engine',
+    team: 'BTSearcher',
+    description: 'Use the Twitter API and ElasticSearch to create a Twitter search feature based on geolocation. The implementation will be able to stream Tweets real-time from Twitter and index/store those tweets into ElasticSearch. The web application will have a good interface for users to enter search information. Finally, this app will be deployed on a cloud-platform.',
+    keywords: 'mongodb, express, react, node.js, mongoose, passport, elasticsearch'
+  },
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || '0.0.0.0',
+  templateEngine: 'react',
+  // Session Cookie settings
+  sessionCookie: {
+    // session expiration is set by default to 24 hours
+    maxAge: 24 * (60 * 60 * 1000),
+    // httpOnly flag makes sure the cookie is only accessed
+    // through the HTTP protocol and not JS/browser
+    httpOnly: true,
+    // secure cookie should be turned to true to provide additional
+    // layer of security so that the cookie is set only when working
+    // in HTTPS mode.
+    secure: false
+  },
+  // sessionSecret should be changed for security measures and concerns
+  sessionSecret: process.env.SESSION_SECRET || 'BTSearcher',
+  // sessionKey is set to the generic sessionId key used by PHP applications
+  // for obsecurity reasons
+  sessionKey: 'sessionId',
+  sessionCollection: 'sessions',
+  logo: 'modules/core/client/img/brand/logo.png',
+  favicon: 'modules/core/client/img/brand/favicon.ico',
+  uploads: {
+    profileUpload: {
+      dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
+      limits: {
+        fileSize: 1*1024*1024 // Max file size in bytes (1 MB)
+      }
+    }
+  }
+};
---
+++ config/env/test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var defaultEnvConfig = require('./default');
+
+module.exports = {
+  log: {
+    // logging with Morgan - https://github.com/expressjs/morgan
+    // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+    format: process.env.LOG_FORMAT || 'combined',
+    options: {
+      // Stream defaults to process.stdout
+      // Uncomment/comment to toggle the logging to a log on the file system
+      stream: {
+        directoryPath: process.cwd(),
+        fileName: 'access.log',
+        rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
+          active: false, // activate to use rotating logs 
+          fileName: 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
+          frequency: 'daily',
+          verbose: false
+        }
+      }
+    }
+  },
+  port: process.env.PORT || 3001,
+  app: {
+    title: defaultEnvConfig.app.title + ' - Test Environment'
+  },
+  // This config is set to true during grunt coverage
+  coverage: process.env.COVERAGE || false
+};
