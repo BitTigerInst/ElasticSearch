6a37310a21dcaf88e6ae2ee91e335317bd3e4ae3
lukeyxu <sdoushi1@gmail.com>
2016-05-28 22:07:40 -0500

    updated front-end form

--- shared/components/Twitsection/Twitsection.jsx
+++ shared/components/Twitsection/Twitsection.jsx
@@ -12,6 +12,7 @@ class Twitsection extends Component {
   }
 
   fetchtweets(){
+
     let url = "/TwitterAPI/search";
     fetch(url, {
       method: 'get',
@@ -33,12 +34,50 @@ class Twitsection extends Component {
     // });
   }
 
+  _handleSubmit(event) {
+    event.preventDefault();
+
+    let city = this._city.value;
+    let query = this._query.value;
+    let url = `/TwitterAPI/searchByArea/${city}/`;
+
+    $.get(url).done(function(data) {
+      console.log(data);
+      this.setState({tweets:data});
+    }.bind(this));
+  }
+
+    // $.ajax({
+    //   method: 'GET',
+    //   url: "/TwitterAPI/search",
+    //   data: city,
+    //   cache: false,
+    //   success: function(data) {
+    //     console.log(data);
+    //     this.setState({tweets: data});
+    //   }.bind(this),
+    //   error: function(xhr, status, err) {
+    //     console.error(this.props.url, status, err.toString());
+    //   }.bind(this)
+    // });
+
   render() {
     return (
-      <div>
+      <div className="body">
         <Header onClick={this.handleClick} />
         <div className="container">
-        	<h1>Twitter APIs Test Section</h1>
+          <form className="tweet-form" onSubmit={this._handleSubmit.bind(this)}>
+            <h1>Twitter Search</h1>
+            <div className="tweet-form-fields">
+              <h4>Please enter the keyword</h4>
+              <input type="text" placeholder="keyword" ref={(input) => this._query = input}/>
+              <h4>Please enter the city</h4>
+              <input type="text" placeholder="city" ref={(input) => this._city = input} />
+            </div>
+            <div className="tweet-form-action">
+            <button type="submit">Submit</button>
+            </div>
+          </form>
           <button className="add-post-button" type="button" onClick={this.fetchtweets.bind(this)}>Fetch tweets</button>
           <div>{JSON.stringify(this.state.tweets)}</div>
         </div>
