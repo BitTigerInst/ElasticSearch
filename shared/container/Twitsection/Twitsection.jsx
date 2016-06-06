0db14bff3a797e04b39d110bff26afd5d70aceda
lukeyxu <sdoushi1@gmail.com>
2016-06-06 08:49:02 -0500

    added google place API feature

--- shared/container/Twitsection/Twitsection.jsx
+++ shared/container/Twitsection/Twitsection.jsx
@@ -33,16 +33,65 @@ class Twitsection extends Component {
     // });
   }
 
+  initMap(dataset) {
+//initial Site
+
+    var initSite = dataset[0];
+
+  //initialize the google map
+    var infowindow;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: initSite,
+      zoom: 5
+    });
+
+  //add the InfoWindow to map
+    infowindow = new google.maps.InfoWindow();
+
+    SitePin();
+    function SitePin(){
+      console.log(dataset);
+      for( var i = 0; i < dataset.length; i++){
+        var marker = new google.maps.Marker({
+          map: map,
+          position: dataset[i]
+        });
+
+        //Service that add content to infowindow
+        google.maps.event.addListener(marker, 'click', function() {
+          var cool = 'nigger!';
+        //infowindow.setContent(place.geometry);
+          infowindow.setContent('<div>test</div>');
+          infowindow.open(map, this);
+        });
+      }
+    }
+}
+
   _handleSubmit(event) {
     event.preventDefault();
-
+    let cleanData;
     let city = this._city.value;
     let query = this._query.value;
     let url = `/TwitterAPI/searchByArea/${city}/`;
 
     $.get(url).done(function(data) {
-      console.log(data);
-      this.setState({tweets:data});
+       cleanData = data.hits.hits.map(function(v) {
+        let lng = v._source.place.bounding_box.coordinates[0][0][0];
+        let lat = v._source.place.bounding_box.coordinates[0][0][1];
+        if (v._source.coordinates !== null) {
+          lat = v._source.coordinates.coordinates[1];
+          lng = v._source.coordinates.coordinates[0];
+        }
+        console.log(lng,lat);
+        return {
+          lat:lat,
+          lng:lng
+        }
+      })
+      console.log(cleanData);
+      this.initMap(cleanData); 
+      this.setState({tweets:cleanData});
     }.bind(this)).fail(function() {
       alert('Error occured!');
     });
@@ -81,6 +130,8 @@ class Twitsection extends Component {
           </form>
           <button className="add-post-button" type="button" onClick={this.fetchtweets.bind(this)}>Fetch tweets</button>
           <div>{JSON.stringify(this.state.tweets)}</div>
+          <div id="map"></div>
+          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChfs6OCo55LNZVJlrncEkg6evXBTi_3g8&signed_in=true&libraries=places" async defer></script>
         </div>
         <Footer />
       </div>
